4f47df1fd7c357421a4afe143438ba2df9a4f47e
lorczyslav <lorczyslav@gmail.com>
2018-05-25 00:47:10 +0200

    Changes to Stack
    Stack is now a template class, all definitions have been moved to the header file

--- GraphAlgorithms/Stack.h
+++ GraphAlgorithms/Stack.h
@@ -1,16 +1,48 @@
 #pragma once
 #include "List.h"
 
+template <typename Type>
 class Stack {
 
 private:
-	List * list;
+	List<Type> * list;
 
 public:
 	Stack();
 	~Stack();
-	void push(const int num);
-	int pop();
+	void push(const Type * data);
+	Type * pop();
 	bool isEmpty();
 };
+
+// ----------------------------
+// Member functions definitions
+// ----------------------------
+
+template <typename Type>
+Stack<Type>::Stack() {
+	this->list = new List<Type>();
+}
+
+template <typename Type>
+Stack<Type>::~Stack() {
+	delete this->list;
+}
+
+//Consider exceptions
+template <typename Type>
+void Stack<Type>::push(const Type * data) {
+	this->list->pushBack(num);
+}
+
+//Consider exceptions
+template <typename Type>
+Type * Stack<Type>::pop() {
+	return this->list->popBack();
+}
+
+template <typename Type>
+bool Stack<Type>::isEmpty() {
+	return this->list->getSize() == 0 ? true : false;
+}
 
