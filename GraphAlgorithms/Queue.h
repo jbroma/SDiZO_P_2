1e38f0a4a01ef0c21c4b1bb2af3b622d378effe0
lorczyslav <lorczyslav@gmail.com>
2018-05-25 00:49:02 +0200

    Changes to Queue
    Queue is now a template class, all definitions have been moved to the header file

--- GraphAlgorithms/Queue.h
+++ GraphAlgorithms/Queue.h
@@ -1,16 +1,46 @@
 #pragma once
 #include "List.h"
 
+template <typename Type>
 class Queue {
 
 private:
-	List * list;
+	List<Type> * list;
 
 public:
 	Queue();
 	~Queue();
-	void enqueue(const int num);
-	int dequeue();
+	void enqueue(const Type * num);
+	Type * dequeue();
 	bool isEmpty();
 };
+
+// ----------------------------
+// Member functions definitions
+// ----------------------------
+
+template <typename Type>
+Queue<Type>::Queue() {
+	this->list = new List<Type>();
+}
+
+template <typename Type>
+Queue<Type>::~Queue() {
+	delete this->list;
+}
+
+template <typename Type>
+void Queue<Type>::enqueue(const Type * num) {
+	this->list->pushFront(num);
+}
+
+template <typename Type>
+Type * Queue<Type>::dequeue() {
+	return this->list->popBack();
+}
+
+template <typename Type>
+bool Queue<Type>::isEmpty() {
+	return this->list->getSize() == 0 ? true : false;
+}
 
